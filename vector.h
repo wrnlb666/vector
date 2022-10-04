72ced8b306cf8ccb7e2015c9b20ac88adcf4e9b6
wrnlb <103810793+wrnlb666@users.noreply.github.com>
2022-10-04 16:24:03 -0400

    Add files via upload

--- vector.h
+++ vector.h
@@ -1,4 +1,4 @@
-//Copyright (c) 2022 Zhang Yixiang
+//Copyright (c) 2022 wrnlb
 //
 //Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
